83df4655b07cdaa5bbd3ae3e214931c0db0445db
aidan <stambaughaidan1@gmail.com>
2024-04-10 14:50:36 -0400

    Add Admin Login page. Add Admin Options page.

--- app/src/main/java/com/example/nfcapp/AdminLoginActivity.java
+++ app/src/main/java/com/example/nfcapp/AdminLoginActivity.java
@@ -10,7 +10,7 @@ public class AdminLoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin);
+        setContentView(R.layout.activity_adminlogin);
 
     }
 
